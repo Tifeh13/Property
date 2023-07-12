07fc90be3ba3bf4c3cc3dede289f436477ca7ada
Afolabi Boluwatife <afolabibolu15@gmail.com>
2023-07-12 20:59:26 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './Pages/Home';
+import Categories from './Pages/Categories';
+import './App.css'
+import Contant from './Pages/Contant';
+// import OffcanvasExample from './Component/OffcanvasExample';
+
+function App() {
+  return (
+    
+      <div>
+        {/* <OffcanvasExample /> */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/categories" element={<Categories />} />
+          <Route path="/contant" element={<Contant />} />
+        </Routes>
+      </div>
+    
+  );
+}
+
+export default App;
