6e651ac1e5e22cebdcea3d0d8d35d9f70ac0a22a
Afolabi Boluwatife <afolabibolu15@gmail.com>
2023-07-18 15:09:56 +0100

    error 5

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,13 @@
-import postcss from 'rollup-plugin-postcss';
+// import postcss from 'rollup-plugin-postcss';
 
-export default {
-  // Your existing Rollup configuration options...
-  plugins: [
-    // Your existing plugins...
-    postcss({
-      extract: true,
-      minimize: true,
-      sourceMap: true,
-    }),
-  ],
-};
+// export default {
+//   // Your existing Rollup configuration options...
+//   plugins: [
+//     // Your existing plugins...
+//     postcss({
+//       extract: true,
+//       minimize: true,
+//       sourceMap: true,
+//     }),
+//   ],
+// };
